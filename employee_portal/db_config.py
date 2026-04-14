87b316eabc36e871121f2735119f9f370f4b8690
chantiasNK26768 <nagendrakondra198@gmail.com>
2026-04-14 11:17:21 +0530

    File_1

---
+++ employee_portal/db_config.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="127.0.0.1",   # ← change from localhost
+        user="root",
+        password="1234567890",
+        database="employee_portal",
+        auth_plugin='mysql_native_password'   # ← ADD THIS LINE
+    )
